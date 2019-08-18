5d2a176708108c805795691b207508177f1d0b32
bayronguti22hope <byrico@live.com>
2019-08-17 19:15:46 -0600

    Messages for changes by choices

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let computerScore = 0 ;
 const userScore_span = document.getElementById("user_score");
 const computerScore_span = document.getElementById("comp_score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_p = document.querySelector(".result > p");
+let result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("rock");
 const paper_div = document.getElementById("paper");
 const scissors_div = document.getElementById("scissors");
@@ -16,49 +16,64 @@ function getComputerChoice() {
     const randomNumber = Math.floor(Math.random() * 3); // Random choice for computer
     return choices[randomNumber]; // the return from the choices for the computer
 }
-
-function win() {
+    // This function will convert the choices into readble words or better look
+    function convertToWord(letter){
+        if (letter === "rock") return "Rock";
+        if (letter === "paper") return "Paper";
+        if (letter === "scissors") return "Scissors";
+        return userScore ;
+    //     // return "Scissors";
+    }
+// Functions for change base on the result (win, lost, draw).
+function win(userChoice, ComputerChoice) {
     userScore++;
     userScore_span.innerHTML = userScore ;
     computerScore_span.innerHTML = computerScore ;
+    const smallWordUser = "user".fontsize(3).sup();
+    const smallWordComp = "comp".fontsize(3).sup();
+    // ES5 way to do it
+    // result_p.innerHTML = convertToWord(userChoice) + " beats " + convertToWord(ComputerChoice) + "." + "You win!"
+    // ES6 way to do it
+    result_p.innerHTML = ` ${convertToWord(userChoice)}${smallWordUser} beats ${convertToWord(ComputerChoice)}${smallWordComp}. You win!`
 }
-function loses(){
+function loses(userChoice, ComputerChoice){
     computerScore++;
-    computerScore_span.innerHTML = computerScore;
     userScore_span.innerHTML = userScore ;
+    computerScore_span.innerHTML = computerScore ;
+    const smallWordUser = "user".fontsize(3).sup();
+    const smallWordComp = "comp".fontsize(3).sup();
+    // ES6 way to do it
+    result_p.innerHTML = ` ${convertToWord(userChoice)}${smallWordUser} loses to ${convertToWord(ComputerChoice)}${smallWordComp}. You lost.`
 }
-function draw(){
-    console.log('this is draw.')
+function draw(userChoice, ComputerChoice){
+    const smallWordUser = "user".fontsize(3).sup();
+    const smallWordComp = "comp".fontsize(3).sup();
+    // ES6 way to do it
+    result_p.innerHTML = ` ${convertToWord(userChoice)}${smallWordUser} equals ${convertToWord(ComputerChoice)}${smallWordComp}. It's draw.`
 }
-
-
-
-
 function game(userChoice){
     const ComputerChoice = getComputerChoice ();
     switch ( userChoice + ComputerChoice){ // Will the comparison between the user & computer choice
-        // Case when the user wins
+      // Case when the user wins
         case "paperrock":
         case "scissorspaper":
         case "rockscissors":
-            win();
+            win(userChoice, ComputerChoice );
             break;
-      // Cases when the computer wins
+      // Cases when the computer lose
         case "rockpaper":
         case "paperscissors":
         case "scissorsrock":
-            loses();
+            loses(userChoice, ComputerChoice );
             break;
       // Cases when is a draw
         case "paperpaper":
         case "rockrock":
         case "scissorsscissors":
-           draw();
-           break;
+            draw(userChoice, ComputerChoice);
+            break;
     }
 }
-
-
 // The events for the when you clicked the bottoms one the options.
 function main(){
     rock_div.addEventListener('click', function(){
